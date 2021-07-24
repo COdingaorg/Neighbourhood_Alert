5026dde093a6adbe4c912a8d953f38f2b476b91d
Caleb Odinga <calemasanga@gmail.com>
2021-07-24 14:50:30 +0300

    adds database- postgresql

--- neighbourhood_alert/settings.py
+++ neighbourhood_alert/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'neighbourhood_alert.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'DB_USER':config('DB_USER'),
+        'DB_PASSWORD':config('DB_PASSWORD'),
+
     }
 }
 
