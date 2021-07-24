12f53fe1996a6f243b269a7ab60f10860af446d5
Caleb Odinga <calemasanga@gmail.com>
2021-07-25 00:54:05 +0300

    adds user profile model to application:

--- hoodalert/models.py
+++ hoodalert/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+  photo_path = models.ImageField(upload_to = 'profiles/')
+  bio = models.CharField(max_length=200)
+  user = models.ForeignKey(User, on_delete=CASCADE)
