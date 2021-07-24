3a79c00c0af5b55975f3025cff099b9ef10d58d9
Caleb Odinga <calemasanga@gmail.com>
2021-07-24 16:59:23 +0300

    adds navbar, footer and css contant variables

--- hoodalert/views.py
+++ hoodalert/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  pass
+  title = 'Home - Neighbourhood Alert'
+
+  context = {
+    'title':title,
+  }
+
+  return render(request, 'all_templates/index.html', context)
