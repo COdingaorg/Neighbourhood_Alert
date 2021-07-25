7706eb9157fcb314516389ed4366dce839fe9cf1
Caleb Odinga <calemasanga@gmail.com>
2021-07-25 13:23:35 +0300

    adds neighbourhood models, adds column admin boolean field

--- hoodalert/models.py
+++ hoodalert/models.py
@@ -7,6 +7,7 @@ class UserProfile(models.Model):
   photo_path = models.ImageField(upload_to = 'profiles/')
   bio = models.CharField(max_length=200)
   user = models.ForeignKey(User, on_delete=CASCADE)
+  admin = models.BooleanField()
 
   def save_profile(self):
     self.save()
@@ -15,4 +16,10 @@ class UserProfile(models.Model):
   def update_profile(cls, id, bio):
     to_update = cls.objects.filter(id = id)
     to_update.update(bio = bio)
-    
+
+class Neighbourhood(models.Model):
+  name = models.CharField(max_length=250)
+  location = models.TextField()
+  population = models.IntegerField()
+  user_prof = models.ForeignKey(UserProfile, on_delete=CASCADE)
+
