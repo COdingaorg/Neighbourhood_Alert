677f610a251b08c1a972738cf53551eed23dba1a
Caleb Odinga <calemasanga@gmail.com>
2021-07-27 08:26:16 +0300

    debugs heroku deployment

--- neighbourhood_alert/settings.py
+++ neighbourhood_alert/settings.py
@@ -155,4 +155,4 @@ MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals)
+django_heroku.settings(locals())
